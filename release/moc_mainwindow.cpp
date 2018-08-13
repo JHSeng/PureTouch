0535c6cd11bb63492b5e2997c6882d0afc1e8459
Pony Brown <31332409+JHSeng@users.noreply.github.com>
2018-08-13 22:43:29 +0800

    Renew a bugfree program

--- release/moc_mainwindow.cpp
+++ release/moc_mainwindow.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[16];
-    char stringdata0[313];
+    QByteArrayData data[59];
+    char stringdata0[1160];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -32,31 +32,98 @@ struct qt_meta_stringdata_MainWindow_t {
 static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
     {
 QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
-QT_MOC_LITERAL(1, 11, 24), // "on_btnLocalMusic_clicked"
-QT_MOC_LITERAL(2, 36, 0), // ""
-QT_MOC_LITERAL(3, 37, 25), // "on_btnMyFavourite_clicked"
-QT_MOC_LITERAL(4, 63, 22), // "on_btnPlayList_clicked"
-QT_MOC_LITERAL(5, 86, 13), // "songInfo_slot"
-QT_MOC_LITERAL(6, 100, 20), // "clearLocalMusic_slot"
-QT_MOC_LITERAL(7, 121, 21), // "clearMyFavourite_slot"
-QT_MOC_LITERAL(8, 143, 18), // "clearPlaylist_slot"
-QT_MOC_LITERAL(9, 162, 17), // "clearAllList_slot"
-QT_MOC_LITERAL(10, 180, 20), // "addToLocalMusic_slot"
-QT_MOC_LITERAL(11, 201, 21), // "addToMyFavourite_slot"
-QT_MOC_LITERAL(12, 223, 18), // "addToPlaylist_slot"
-QT_MOC_LITERAL(13, 242, 20), // "setBackGround_1_slot"
-QT_MOC_LITERAL(14, 263, 20), // "setBackGround_2_slot"
-QT_MOC_LITERAL(15, 284, 28) // "setBackground_customize_slot"
+QT_MOC_LITERAL(1, 11, 16), // "signal_showAbout"
+QT_MOC_LITERAL(2, 28, 0), // ""
+QT_MOC_LITERAL(3, 29, 24), // "on_btnLocalMusic_clicked"
+QT_MOC_LITERAL(4, 54, 25), // "on_btnMyFavourite_clicked"
+QT_MOC_LITERAL(5, 80, 22), // "on_btnPlayList_clicked"
+QT_MOC_LITERAL(6, 103, 13), // "songInfo_slot"
+QT_MOC_LITERAL(7, 117, 20), // "clearLocalMusic_slot"
+QT_MOC_LITERAL(8, 138, 21), // "clearMyFavourite_slot"
+QT_MOC_LITERAL(9, 160, 18), // "clearPlaylist_slot"
+QT_MOC_LITERAL(10, 179, 17), // "clearAllList_slot"
+QT_MOC_LITERAL(11, 197, 20), // "addToLocalMusic_slot"
+QT_MOC_LITERAL(12, 218, 21), // "addToMyFavourite_slot"
+QT_MOC_LITERAL(13, 240, 18), // "addToPlaylist_slot"
+QT_MOC_LITERAL(14, 259, 20), // "setBackGround_1_slot"
+QT_MOC_LITERAL(15, 280, 20), // "setBackGround_2_slot"
+QT_MOC_LITERAL(16, 301, 28), // "setBackground_customize_slot"
+QT_MOC_LITERAL(17, 330, 12), // "preSong_slot"
+QT_MOC_LITERAL(18, 343, 13), // "nextSong_slot"
+QT_MOC_LITERAL(19, 357, 16), // "playOrPause_slot"
+QT_MOC_LITERAL(20, 374, 16), // "playInOrder_slot"
+QT_MOC_LITERAL(21, 391, 16), // "playShuffle_slot"
+QT_MOC_LITERAL(22, 408, 20), // "playSingleCycle_slot"
+QT_MOC_LITERAL(23, 429, 16), // "musicStateChange"
+QT_MOC_LITERAL(24, 446, 19), // "QMediaPlayer::State"
+QT_MOC_LITERAL(25, 466, 5), // "state"
+QT_MOC_LITERAL(26, 472, 16), // "contextMenuEvent"
+QT_MOC_LITERAL(27, 489, 18), // "QContextMenuEvent*"
+QT_MOC_LITERAL(28, 508, 5), // "event"
+QT_MOC_LITERAL(29, 514, 12), // "Action1_slot"
+QT_MOC_LITERAL(30, 527, 12), // "Action2_slot"
+QT_MOC_LITERAL(31, 540, 12), // "Action3_slot"
+QT_MOC_LITERAL(32, 553, 12), // "Action4_slot"
+QT_MOC_LITERAL(33, 566, 12), // "Action5_slot"
+QT_MOC_LITERAL(34, 579, 14), // "Action1_2_slot"
+QT_MOC_LITERAL(35, 594, 14), // "Action2_2_slot"
+QT_MOC_LITERAL(36, 609, 14), // "Action3_2_slot"
+QT_MOC_LITERAL(37, 624, 14), // "Action4_2_slot"
+QT_MOC_LITERAL(38, 639, 14), // "Action1_3_slot"
+QT_MOC_LITERAL(39, 654, 14), // "Action2_3_slot"
+QT_MOC_LITERAL(40, 669, 14), // "Action3_3_slot"
+QT_MOC_LITERAL(41, 684, 25), // "on_btnPlayOrPause_clicked"
+QT_MOC_LITERAL(42, 710, 26), // "on_btnPlayNextSong_clicked"
+QT_MOC_LITERAL(43, 737, 25), // "on_btnPlayPreSong_clicked"
+QT_MOC_LITERAL(44, 763, 28), // "on_btnShowOrHideList_clicked"
+QT_MOC_LITERAL(45, 792, 28), // "on_btnSelectPlayMode_clicked"
+QT_MOC_LITERAL(46, 821, 31), // "on_localMusicList_doubleClicked"
+QT_MOC_LITERAL(47, 853, 11), // "QModelIndex"
+QT_MOC_LITERAL(48, 865, 5), // "index"
+QT_MOC_LITERAL(49, 871, 32), // "on_myFavouriteList_doubleClicked"
+QT_MOC_LITERAL(50, 904, 29), // "on_playlistList_doubleClicked"
+QT_MOC_LITERAL(51, 934, 22), // "on_btnMinimize_clicked"
+QT_MOC_LITERAL(52, 957, 19), // "on_btnClose_clicked"
+QT_MOC_LITERAL(53, 977, 20), // "on_btnVolume_clicked"
+QT_MOC_LITERAL(54, 998, 44), // "on_localMusicList_customConte..."
+QT_MOC_LITERAL(55, 1043, 3), // "pos"
+QT_MOC_LITERAL(56, 1047, 45), // "on_myFavouriteList_customCont..."
+QT_MOC_LITERAL(57, 1093, 42), // "on_playlistList_customContext..."
+QT_MOC_LITERAL(58, 1136, 23) // "on_btnMiniStyle_clicked"
 
     },
-    "MainWindow\0on_btnLocalMusic_clicked\0"
-    "\0on_btnMyFavourite_clicked\0"
+    "MainWindow\0signal_showAbout\0\0"
+    "on_btnLocalMusic_clicked\0"
+    "on_btnMyFavourite_clicked\0"
     "on_btnPlayList_clicked\0songInfo_slot\0"
     "clearLocalMusic_slot\0clearMyFavourite_slot\0"
     "clearPlaylist_slot\0clearAllList_slot\0"
     "addToLocalMusic_slot\0addToMyFavourite_slot\0"
     "addToPlaylist_slot\0setBackGround_1_slot\0"
-    "setBackGround_2_slot\0setBackground_customize_slot"
+    "setBackGround_2_slot\0setBackground_customize_slot\0"
+    "preSong_slot\0nextSong_slot\0playOrPause_slot\0"
+    "playInOrder_slot\0playShuffle_slot\0"
+    "playSingleCycle_slot\0musicStateChange\0"
+    "QMediaPlayer::State\0state\0contextMenuEvent\0"
+    "QContextMenuEvent*\0event\0Action1_slot\0"
+    "Action2_slot\0Action3_slot\0Action4_slot\0"
+    "Action5_slot\0Action1_2_slot\0Action2_2_slot\0"
+    "Action3_2_slot\0Action4_2_slot\0"
+    "Action1_3_slot\0Action2_3_slot\0"
+    "Action3_3_slot\0on_btnPlayOrPause_clicked\0"
+    "on_btnPlayNextSong_clicked\0"
+    "on_btnPlayPreSong_clicked\0"
+    "on_btnShowOrHideList_clicked\0"
+    "on_btnSelectPlayMode_clicked\0"
+    "on_localMusicList_doubleClicked\0"
+    "QModelIndex\0index\0on_myFavouriteList_doubleClicked\0"
+    "on_playlistList_doubleClicked\0"
+    "on_btnMinimize_clicked\0on_btnClose_clicked\0"
+    "on_btnVolume_clicked\0"
+    "on_localMusicList_customContextMenuRequested\0"
+    "pos\0on_myFavouriteList_customContextMenuRequested\0"
+    "on_playlistList_customContextMenuRequested\0"
+    "on_btnMiniStyle_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -66,28 +133,69 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-      14,   14, // methods
+      50,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
-       0,       // signalCount
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    0,  264,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   84,    2, 0x08 /* Private */,
-       3,    0,   85,    2, 0x08 /* Private */,
-       4,    0,   86,    2, 0x08 /* Private */,
-       5,    0,   87,    2, 0x08 /* Private */,
-       6,    0,   88,    2, 0x08 /* Private */,
-       7,    0,   89,    2, 0x08 /* Private */,
-       8,    0,   90,    2, 0x08 /* Private */,
-       9,    0,   91,    2, 0x08 /* Private */,
-      10,    0,   92,    2, 0x08 /* Private */,
-      11,    0,   93,    2, 0x08 /* Private */,
-      12,    0,   94,    2, 0x08 /* Private */,
-      13,    0,   95,    2, 0x08 /* Private */,
-      14,    0,   96,    2, 0x08 /* Private */,
-      15,    0,   97,    2, 0x08 /* Private */,
+       3,    0,  265,    2, 0x08 /* Private */,
+       4,    0,  266,    2, 0x08 /* Private */,
+       5,    0,  267,    2, 0x08 /* Private */,
+       6,    0,  268,    2, 0x08 /* Private */,
+       7,    0,  269,    2, 0x08 /* Private */,
+       8,    0,  270,    2, 0x08 /* Private */,
+       9,    0,  271,    2, 0x08 /* Private */,
+      10,    0,  272,    2, 0x08 /* Private */,
+      11,    0,  273,    2, 0x08 /* Private */,
+      12,    0,  274,    2, 0x08 /* Private */,
+      13,    0,  275,    2, 0x08 /* Private */,
+      14,    0,  276,    2, 0x08 /* Private */,
+      15,    0,  277,    2, 0x08 /* Private */,
+      16,    0,  278,    2, 0x08 /* Private */,
+      17,    0,  279,    2, 0x08 /* Private */,
+      18,    0,  280,    2, 0x08 /* Private */,
+      19,    0,  281,    2, 0x08 /* Private */,
+      20,    0,  282,    2, 0x08 /* Private */,
+      21,    0,  283,    2, 0x08 /* Private */,
+      22,    0,  284,    2, 0x08 /* Private */,
+      23,    1,  285,    2, 0x08 /* Private */,
+      26,    1,  288,    2, 0x08 /* Private */,
+      29,    0,  291,    2, 0x08 /* Private */,
+      30,    0,  292,    2, 0x08 /* Private */,
+      31,    0,  293,    2, 0x08 /* Private */,
+      32,    0,  294,    2, 0x08 /* Private */,
+      33,    0,  295,    2, 0x08 /* Private */,
+      34,    0,  296,    2, 0x08 /* Private */,
+      35,    0,  297,    2, 0x08 /* Private */,
+      36,    0,  298,    2, 0x08 /* Private */,
+      37,    0,  299,    2, 0x08 /* Private */,
+      38,    0,  300,    2, 0x08 /* Private */,
+      39,    0,  301,    2, 0x08 /* Private */,
+      40,    0,  302,    2, 0x08 /* Private */,
+      41,    0,  303,    2, 0x08 /* Private */,
+      42,    0,  304,    2, 0x08 /* Private */,
+      43,    0,  305,    2, 0x08 /* Private */,
+      44,    0,  306,    2, 0x08 /* Private */,
+      45,    0,  307,    2, 0x08 /* Private */,
+      46,    1,  308,    2, 0x08 /* Private */,
+      49,    1,  311,    2, 0x08 /* Private */,
+      50,    1,  314,    2, 0x08 /* Private */,
+      51,    0,  317,    2, 0x08 /* Private */,
+      52,    0,  318,    2, 0x08 /* Private */,
+      53,    0,  319,    2, 0x08 /* Private */,
+      54,    1,  320,    2, 0x08 /* Private */,
+      56,    1,  323,    2, 0x08 /* Private */,
+      57,    1,  326,    2, 0x08 /* Private */,
+      58,    0,  329,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void,
 
  // slots: parameters
     QMetaType::Void,
@@ -104,6 +212,41 @@ static const uint qt_meta_data_MainWindow[] = {
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 24,   25,
+    QMetaType::Void, 0x80000000 | 27,   28,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 47,   48,
+    QMetaType::Void, 0x80000000 | 47,   48,
+    QMetaType::Void, 0x80000000 | 47,   48,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QPoint,   55,
+    QMetaType::Void, QMetaType::QPoint,   55,
+    QMetaType::Void, QMetaType::QPoint,   55,
+    QMetaType::Void,
 
        0        // eod
 };
@@ -114,24 +257,79 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         MainWindow *_t = static_cast<MainWindow *>(_o);
         Q_UNUSED(_t)
         switch (_id) {
-        case 0: _t->on_btnLocalMusic_clicked(); break;
-        case 1: _t->on_btnMyFavourite_clicked(); break;
-        case 2: _t->on_btnPlayList_clicked(); break;
-        case 3: _t->songInfo_slot(); break;
-        case 4: _t->clearLocalMusic_slot(); break;
-        case 5: _t->clearMyFavourite_slot(); break;
-        case 6: _t->clearPlaylist_slot(); break;
-        case 7: _t->clearAllList_slot(); break;
-        case 8: _t->addToLocalMusic_slot(); break;
-        case 9: _t->addToMyFavourite_slot(); break;
-        case 10: _t->addToPlaylist_slot(); break;
-        case 11: _t->setBackGround_1_slot(); break;
-        case 12: _t->setBackGround_2_slot(); break;
-        case 13: _t->setBackground_customize_slot(); break;
+        case 0: _t->signal_showAbout(); break;
+        case 1: _t->on_btnLocalMusic_clicked(); break;
+        case 2: _t->on_btnMyFavourite_clicked(); break;
+        case 3: _t->on_btnPlayList_clicked(); break;
+        case 4: _t->songInfo_slot(); break;
+        case 5: _t->clearLocalMusic_slot(); break;
+        case 6: _t->clearMyFavourite_slot(); break;
+        case 7: _t->clearPlaylist_slot(); break;
+        case 8: _t->clearAllList_slot(); break;
+        case 9: _t->addToLocalMusic_slot(); break;
+        case 10: _t->addToMyFavourite_slot(); break;
+        case 11: _t->addToPlaylist_slot(); break;
+        case 12: _t->setBackGround_1_slot(); break;
+        case 13: _t->setBackGround_2_slot(); break;
+        case 14: _t->setBackground_customize_slot(); break;
+        case 15: _t->preSong_slot(); break;
+        case 16: _t->nextSong_slot(); break;
+        case 17: _t->playOrPause_slot(); break;
+        case 18: _t->playInOrder_slot(); break;
+        case 19: _t->playShuffle_slot(); break;
+        case 20: _t->playSingleCycle_slot(); break;
+        case 21: _t->musicStateChange((*reinterpret_cast< QMediaPlayer::State(*)>(_a[1]))); break;
+        case 22: _t->contextMenuEvent((*reinterpret_cast< QContextMenuEvent*(*)>(_a[1]))); break;
+        case 23: _t->Action1_slot(); break;
+        case 24: _t->Action2_slot(); break;
+        case 25: _t->Action3_slot(); break;
+        case 26: _t->Action4_slot(); break;
+        case 27: _t->Action5_slot(); break;
+        case 28: _t->Action1_2_slot(); break;
+        case 29: _t->Action2_2_slot(); break;
+        case 30: _t->Action3_2_slot(); break;
+        case 31: _t->Action4_2_slot(); break;
+        case 32: _t->Action1_3_slot(); break;
+        case 33: _t->Action2_3_slot(); break;
+        case 34: _t->Action3_3_slot(); break;
+        case 35: _t->on_btnPlayOrPause_clicked(); break;
+        case 36: _t->on_btnPlayNextSong_clicked(); break;
+        case 37: _t->on_btnPlayPreSong_clicked(); break;
+        case 38: _t->on_btnShowOrHideList_clicked(); break;
+        case 39: _t->on_btnSelectPlayMode_clicked(); break;
+        case 40: _t->on_localMusicList_doubleClicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        case 41: _t->on_myFavouriteList_doubleClicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        case 42: _t->on_playlistList_doubleClicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        case 43: _t->on_btnMinimize_clicked(); break;
+        case 44: _t->on_btnClose_clicked(); break;
+        case 45: _t->on_btnVolume_clicked(); break;
+        case 46: _t->on_localMusicList_customContextMenuRequested((*reinterpret_cast< const QPoint(*)>(_a[1]))); break;
+        case 47: _t->on_myFavouriteList_customContextMenuRequested((*reinterpret_cast< const QPoint(*)>(_a[1]))); break;
+        case 48: _t->on_playlistList_customContextMenuRequested((*reinterpret_cast< const QPoint(*)>(_a[1]))); break;
+        case 49: _t->on_btnMiniStyle_clicked(); break;
         default: ;
         }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 21:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QMediaPlayer::State >(); break;
+            }
+            break;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (MainWindow::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MainWindow::signal_showAbout)) {
+                *result = 0;
+                return;
+            }
+        }
     }
-    Q_UNUSED(_a);
 }
 
 QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = {
@@ -159,15 +357,21 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 14)
+        if (_id < 50)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 14;
+        _id -= 50;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 14)
-            *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 14;
+        if (_id < 50)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 50;
     }
     return _id;
+}
+
+// SIGNAL 0
+void MainWindow::signal_showAbout()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, nullptr);
 }
 QT_WARNING_POP
 QT_END_MOC_NAMESPACE
