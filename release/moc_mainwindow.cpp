d7e27caca983e1118ac04a3cc6654a94294fc6eb
Pony Brown <31332409+JHSeng@users.noreply.github.com>
2018-08-14 23:29:47 +0800

    Renew program

--- release/moc_mainwindow.cpp
+++ release/moc_mainwindow.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[59];
-    char stringdata0[1160];
+    QByteArrayData data[77];
+    char stringdata0[1474];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -72,24 +72,42 @@ QT_MOC_LITERAL(37, 624, 14), // "Action4_2_slot"
 QT_MOC_LITERAL(38, 639, 14), // "Action1_3_slot"
 QT_MOC_LITERAL(39, 654, 14), // "Action2_3_slot"
 QT_MOC_LITERAL(40, 669, 14), // "Action3_3_slot"
-QT_MOC_LITERAL(41, 684, 25), // "on_btnPlayOrPause_clicked"
-QT_MOC_LITERAL(42, 710, 26), // "on_btnPlayNextSong_clicked"
-QT_MOC_LITERAL(43, 737, 25), // "on_btnPlayPreSong_clicked"
-QT_MOC_LITERAL(44, 763, 28), // "on_btnShowOrHideList_clicked"
-QT_MOC_LITERAL(45, 792, 28), // "on_btnSelectPlayMode_clicked"
-QT_MOC_LITERAL(46, 821, 31), // "on_localMusicList_doubleClicked"
-QT_MOC_LITERAL(47, 853, 11), // "QModelIndex"
-QT_MOC_LITERAL(48, 865, 5), // "index"
-QT_MOC_LITERAL(49, 871, 32), // "on_myFavouriteList_doubleClicked"
-QT_MOC_LITERAL(50, 904, 29), // "on_playlistList_doubleClicked"
-QT_MOC_LITERAL(51, 934, 22), // "on_btnMinimize_clicked"
-QT_MOC_LITERAL(52, 957, 19), // "on_btnClose_clicked"
-QT_MOC_LITERAL(53, 977, 20), // "on_btnVolume_clicked"
-QT_MOC_LITERAL(54, 998, 44), // "on_localMusicList_customConte..."
-QT_MOC_LITERAL(55, 1043, 3), // "pos"
-QT_MOC_LITERAL(56, 1047, 45), // "on_myFavouriteList_customCont..."
-QT_MOC_LITERAL(57, 1093, 42), // "on_playlistList_customContext..."
-QT_MOC_LITERAL(58, 1136, 23) // "on_btnMiniStyle_clicked"
+QT_MOC_LITERAL(41, 684, 11), // "Action_slot"
+QT_MOC_LITERAL(42, 696, 13), // "Action_1_slot"
+QT_MOC_LITERAL(43, 710, 13), // "Action_2_slot"
+QT_MOC_LITERAL(44, 724, 13), // "Action_3_slot"
+QT_MOC_LITERAL(45, 738, 13), // "Action_4_slot"
+QT_MOC_LITERAL(46, 752, 25), // "on_btnPlayOrPause_clicked"
+QT_MOC_LITERAL(47, 778, 26), // "on_btnPlayNextSong_clicked"
+QT_MOC_LITERAL(48, 805, 25), // "on_btnPlayPreSong_clicked"
+QT_MOC_LITERAL(49, 831, 28), // "on_btnShowOrHideList_clicked"
+QT_MOC_LITERAL(50, 860, 28), // "on_btnSelectPlayMode_clicked"
+QT_MOC_LITERAL(51, 889, 31), // "on_localMusicList_doubleClicked"
+QT_MOC_LITERAL(52, 921, 11), // "QModelIndex"
+QT_MOC_LITERAL(53, 933, 5), // "index"
+QT_MOC_LITERAL(54, 939, 32), // "on_myFavouriteList_doubleClicked"
+QT_MOC_LITERAL(55, 972, 29), // "on_playlistList_doubleClicked"
+QT_MOC_LITERAL(56, 1002, 22), // "on_btnMinimize_clicked"
+QT_MOC_LITERAL(57, 1025, 19), // "on_btnClose_clicked"
+QT_MOC_LITERAL(58, 1045, 20), // "on_btnVolume_clicked"
+QT_MOC_LITERAL(59, 1066, 44), // "on_localMusicList_customConte..."
+QT_MOC_LITERAL(60, 1111, 3), // "pos"
+QT_MOC_LITERAL(61, 1115, 45), // "on_myFavouriteList_customCont..."
+QT_MOC_LITERAL(62, 1161, 42), // "on_playlistList_customContext..."
+QT_MOC_LITERAL(63, 1204, 23), // "on_btnMiniStyle_clicked"
+QT_MOC_LITERAL(64, 1228, 9), // "Data_slot"
+QT_MOC_LITERAL(65, 1238, 3), // "row"
+QT_MOC_LITERAL(66, 1242, 8), // "position"
+QT_MOC_LITERAL(67, 1251, 15), // "QMediaPlaylist*"
+QT_MOC_LITERAL(68, 1267, 4), // "list"
+QT_MOC_LITERAL(69, 1272, 28), // "QMediaPlaylist::PlaybackMode"
+QT_MOC_LITERAL(70, 1301, 5), // "model"
+QT_MOC_LITERAL(71, 1307, 30), // "on_btnAddToCurrentList_clicked"
+QT_MOC_LITERAL(72, 1338, 20), // "on_btnSearch_clicked"
+QT_MOC_LITERAL(73, 1359, 27), // "on_searchList_doubleClicked"
+QT_MOC_LITERAL(74, 1387, 40), // "on_searchList_customContextMe..."
+QT_MOC_LITERAL(75, 1428, 26), // "on_searchBar_returnPressed"
+QT_MOC_LITERAL(76, 1455, 18) // "PlaylistModel_slot"
 
     },
     "MainWindow\0signal_showAbout\0\0"
@@ -110,7 +128,9 @@ QT_MOC_LITERAL(58, 1136, 23) // "on_btnMiniStyle_clicked"
     "Action5_slot\0Action1_2_slot\0Action2_2_slot\0"
     "Action3_2_slot\0Action4_2_slot\0"
     "Action1_3_slot\0Action2_3_slot\0"
-    "Action3_3_slot\0on_btnPlayOrPause_clicked\0"
+    "Action3_3_slot\0Action_slot\0Action_1_slot\0"
+    "Action_2_slot\0Action_3_slot\0Action_4_slot\0"
+    "on_btnPlayOrPause_clicked\0"
     "on_btnPlayNextSong_clicked\0"
     "on_btnPlayPreSong_clicked\0"
     "on_btnShowOrHideList_clicked\0"
@@ -123,7 +143,14 @@ QT_MOC_LITERAL(58, 1136, 23) // "on_btnMiniStyle_clicked"
     "on_localMusicList_customContextMenuRequested\0"
     "pos\0on_myFavouriteList_customContextMenuRequested\0"
     "on_playlistList_customContextMenuRequested\0"
-    "on_btnMiniStyle_clicked"
+    "on_btnMiniStyle_clicked\0Data_slot\0row\0"
+    "position\0QMediaPlaylist*\0list\0"
+    "QMediaPlaylist::PlaybackMode\0model\0"
+    "on_btnAddToCurrentList_clicked\0"
+    "on_btnSearch_clicked\0on_searchList_doubleClicked\0"
+    "on_searchList_customContextMenuRequested\0"
+    "on_searchBar_returnPressed\0"
+    "PlaylistModel_slot"
 };
 #undef QT_MOC_LITERAL
 
@@ -133,7 +160,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-      50,   14, // methods
+      62,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -141,58 +168,70 @@ static const uint qt_meta_data_MainWindow[] = {
        1,       // signalCount
 
  // signals: name, argc, parameters, tag, flags
-       1,    0,  264,    2, 0x06 /* Public */,
+       1,    0,  324,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       3,    0,  265,    2, 0x08 /* Private */,
-       4,    0,  266,    2, 0x08 /* Private */,
-       5,    0,  267,    2, 0x08 /* Private */,
-       6,    0,  268,    2, 0x08 /* Private */,
-       7,    0,  269,    2, 0x08 /* Private */,
-       8,    0,  270,    2, 0x08 /* Private */,
-       9,    0,  271,    2, 0x08 /* Private */,
-      10,    0,  272,    2, 0x08 /* Private */,
-      11,    0,  273,    2, 0x08 /* Private */,
-      12,    0,  274,    2, 0x08 /* Private */,
-      13,    0,  275,    2, 0x08 /* Private */,
-      14,    0,  276,    2, 0x08 /* Private */,
-      15,    0,  277,    2, 0x08 /* Private */,
-      16,    0,  278,    2, 0x08 /* Private */,
-      17,    0,  279,    2, 0x08 /* Private */,
-      18,    0,  280,    2, 0x08 /* Private */,
-      19,    0,  281,    2, 0x08 /* Private */,
-      20,    0,  282,    2, 0x08 /* Private */,
-      21,    0,  283,    2, 0x08 /* Private */,
-      22,    0,  284,    2, 0x08 /* Private */,
-      23,    1,  285,    2, 0x08 /* Private */,
-      26,    1,  288,    2, 0x08 /* Private */,
-      29,    0,  291,    2, 0x08 /* Private */,
-      30,    0,  292,    2, 0x08 /* Private */,
-      31,    0,  293,    2, 0x08 /* Private */,
-      32,    0,  294,    2, 0x08 /* Private */,
-      33,    0,  295,    2, 0x08 /* Private */,
-      34,    0,  296,    2, 0x08 /* Private */,
-      35,    0,  297,    2, 0x08 /* Private */,
-      36,    0,  298,    2, 0x08 /* Private */,
-      37,    0,  299,    2, 0x08 /* Private */,
-      38,    0,  300,    2, 0x08 /* Private */,
-      39,    0,  301,    2, 0x08 /* Private */,
-      40,    0,  302,    2, 0x08 /* Private */,
-      41,    0,  303,    2, 0x08 /* Private */,
-      42,    0,  304,    2, 0x08 /* Private */,
-      43,    0,  305,    2, 0x08 /* Private */,
-      44,    0,  306,    2, 0x08 /* Private */,
-      45,    0,  307,    2, 0x08 /* Private */,
-      46,    1,  308,    2, 0x08 /* Private */,
-      49,    1,  311,    2, 0x08 /* Private */,
-      50,    1,  314,    2, 0x08 /* Private */,
-      51,    0,  317,    2, 0x08 /* Private */,
-      52,    0,  318,    2, 0x08 /* Private */,
-      53,    0,  319,    2, 0x08 /* Private */,
-      54,    1,  320,    2, 0x08 /* Private */,
-      56,    1,  323,    2, 0x08 /* Private */,
-      57,    1,  326,    2, 0x08 /* Private */,
-      58,    0,  329,    2, 0x08 /* Private */,
+       3,    0,  325,    2, 0x08 /* Private */,
+       4,    0,  326,    2, 0x08 /* Private */,
+       5,    0,  327,    2, 0x08 /* Private */,
+       6,    0,  328,    2, 0x08 /* Private */,
+       7,    0,  329,    2, 0x08 /* Private */,
+       8,    0,  330,    2, 0x08 /* Private */,
+       9,    0,  331,    2, 0x08 /* Private */,
+      10,    0,  332,    2, 0x08 /* Private */,
+      11,    0,  333,    2, 0x08 /* Private */,
+      12,    0,  334,    2, 0x08 /* Private */,
+      13,    0,  335,    2, 0x08 /* Private */,
+      14,    0,  336,    2, 0x08 /* Private */,
+      15,    0,  337,    2, 0x08 /* Private */,
+      16,    0,  338,    2, 0x08 /* Private */,
+      17,    0,  339,    2, 0x08 /* Private */,
+      18,    0,  340,    2, 0x08 /* Private */,
+      19,    0,  341,    2, 0x08 /* Private */,
+      20,    0,  342,    2, 0x08 /* Private */,
+      21,    0,  343,    2, 0x08 /* Private */,
+      22,    0,  344,    2, 0x08 /* Private */,
+      23,    1,  345,    2, 0x08 /* Private */,
+      26,    1,  348,    2, 0x08 /* Private */,
+      29,    0,  351,    2, 0x08 /* Private */,
+      30,    0,  352,    2, 0x08 /* Private */,
+      31,    0,  353,    2, 0x08 /* Private */,
+      32,    0,  354,    2, 0x08 /* Private */,
+      33,    0,  355,    2, 0x08 /* Private */,
+      34,    0,  356,    2, 0x08 /* Private */,
+      35,    0,  357,    2, 0x08 /* Private */,
+      36,    0,  358,    2, 0x08 /* Private */,
+      37,    0,  359,    2, 0x08 /* Private */,
+      38,    0,  360,    2, 0x08 /* Private */,
+      39,    0,  361,    2, 0x08 /* Private */,
+      40,    0,  362,    2, 0x08 /* Private */,
+      41,    0,  363,    2, 0x08 /* Private */,
+      42,    0,  364,    2, 0x08 /* Private */,
+      43,    0,  365,    2, 0x08 /* Private */,
+      44,    0,  366,    2, 0x08 /* Private */,
+      45,    0,  367,    2, 0x08 /* Private */,
+      46,    0,  368,    2, 0x08 /* Private */,
+      47,    0,  369,    2, 0x08 /* Private */,
+      48,    0,  370,    2, 0x08 /* Private */,
+      49,    0,  371,    2, 0x08 /* Private */,
+      50,    0,  372,    2, 0x08 /* Private */,
+      51,    1,  373,    2, 0x08 /* Private */,
+      54,    1,  376,    2, 0x08 /* Private */,
+      55,    1,  379,    2, 0x08 /* Private */,
+      56,    0,  382,    2, 0x08 /* Private */,
+      57,    0,  383,    2, 0x08 /* Private */,
+      58,    0,  384,    2, 0x08 /* Private */,
+      59,    1,  385,    2, 0x08 /* Private */,
+      61,    1,  388,    2, 0x08 /* Private */,
+      62,    1,  391,    2, 0x08 /* Private */,
+      63,    0,  394,    2, 0x08 /* Private */,
+      64,    5,  395,    2, 0x08 /* Private */,
+      71,    0,  406,    2, 0x08 /* Private */,
+      72,    0,  407,    2, 0x08 /* Private */,
+      73,    1,  408,    2, 0x08 /* Private */,
+      74,    1,  411,    2, 0x08 /* Private */,
+      75,    0,  414,    2, 0x08 /* Private */,
+      76,    1,  415,    2, 0x08 /* Private */,
 
  // signals: parameters
     QMetaType::Void,
@@ -237,16 +276,28 @@ static const uint qt_meta_data_MainWindow[] = {
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void, 0x80000000 | 47,   48,
-    QMetaType::Void, 0x80000000 | 47,   48,
-    QMetaType::Void, 0x80000000 | 47,   48,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void, QMetaType::QPoint,   55,
-    QMetaType::Void, QMetaType::QPoint,   55,
-    QMetaType::Void, QMetaType::QPoint,   55,
     QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 52,   53,
+    QMetaType::Void, 0x80000000 | 52,   53,
+    QMetaType::Void, 0x80000000 | 52,   53,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QPoint,   60,
+    QMetaType::Void, QMetaType::QPoint,   60,
+    QMetaType::Void, QMetaType::QPoint,   60,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int, QMetaType::Int, 0x80000000 | 67, 0x80000000 | 24, 0x80000000 | 69,   65,   66,   68,   25,   70,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 52,   53,
+    QMetaType::Void, QMetaType::QPoint,   60,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 69,   70,
 
        0        // eod
 };
@@ -292,21 +343,33 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 32: _t->Action1_3_slot(); break;
         case 33: _t->Action2_3_slot(); break;
         case 34: _t->Action3_3_slot(); break;
-        case 35: _t->on_btnPlayOrPause_clicked(); break;
-        case 36: _t->on_btnPlayNextSong_clicked(); break;
-        case 37: _t->on_btnPlayPreSong_clicked(); break;
-        case 38: _t->on_btnShowOrHideList_clicked(); break;
-        case 39: _t->on_btnSelectPlayMode_clicked(); break;
-        case 40: _t->on_localMusicList_doubleClicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
-        case 41: _t->on_myFavouriteList_doubleClicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
-        case 42: _t->on_playlistList_doubleClicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
-        case 43: _t->on_btnMinimize_clicked(); break;
-        case 44: _t->on_btnClose_clicked(); break;
-        case 45: _t->on_btnVolume_clicked(); break;
-        case 46: _t->on_localMusicList_customContextMenuRequested((*reinterpret_cast< const QPoint(*)>(_a[1]))); break;
-        case 47: _t->on_myFavouriteList_customContextMenuRequested((*reinterpret_cast< const QPoint(*)>(_a[1]))); break;
-        case 48: _t->on_playlistList_customContextMenuRequested((*reinterpret_cast< const QPoint(*)>(_a[1]))); break;
-        case 49: _t->on_btnMiniStyle_clicked(); break;
+        case 35: _t->Action_slot(); break;
+        case 36: _t->Action_1_slot(); break;
+        case 37: _t->Action_2_slot(); break;
+        case 38: _t->Action_3_slot(); break;
+        case 39: _t->Action_4_slot(); break;
+        case 40: _t->on_btnPlayOrPause_clicked(); break;
+        case 41: _t->on_btnPlayNextSong_clicked(); break;
+        case 42: _t->on_btnPlayPreSong_clicked(); break;
+        case 43: _t->on_btnShowOrHideList_clicked(); break;
+        case 44: _t->on_btnSelectPlayMode_clicked(); break;
+        case 45: _t->on_localMusicList_doubleClicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        case 46: _t->on_myFavouriteList_doubleClicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        case 47: _t->on_playlistList_doubleClicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        case 48: _t->on_btnMinimize_clicked(); break;
+        case 49: _t->on_btnClose_clicked(); break;
+        case 50: _t->on_btnVolume_clicked(); break;
+        case 51: _t->on_localMusicList_customContextMenuRequested((*reinterpret_cast< const QPoint(*)>(_a[1]))); break;
+        case 52: _t->on_myFavouriteList_customContextMenuRequested((*reinterpret_cast< const QPoint(*)>(_a[1]))); break;
+        case 53: _t->on_playlistList_customContextMenuRequested((*reinterpret_cast< const QPoint(*)>(_a[1]))); break;
+        case 54: _t->on_btnMiniStyle_clicked(); break;
+        case 55: _t->Data_slot((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2])),(*reinterpret_cast< QMediaPlaylist*(*)>(_a[3])),(*reinterpret_cast< QMediaPlayer::State(*)>(_a[4])),(*reinterpret_cast< QMediaPlaylist::PlaybackMode(*)>(_a[5]))); break;
+        case 56: _t->on_btnAddToCurrentList_clicked(); break;
+        case 57: _t->on_btnSearch_clicked(); break;
+        case 58: _t->on_searchList_doubleClicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        case 59: _t->on_searchList_customContextMenuRequested((*reinterpret_cast< const QPoint(*)>(_a[1]))); break;
+        case 60: _t->on_searchBar_returnPressed(); break;
+        case 61: _t->PlaylistModel_slot((*reinterpret_cast< QMediaPlaylist::PlaybackMode(*)>(_a[1]))); break;
         default: ;
         }
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
@@ -317,6 +380,24 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
             default: *reinterpret_cast<int*>(_a[0]) = -1; break;
             case 0:
                 *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QMediaPlayer::State >(); break;
+            }
+            break;
+        case 55:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 3:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QMediaPlayer::State >(); break;
+            case 2:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QMediaPlaylist* >(); break;
+            case 4:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QMediaPlaylist::PlaybackMode >(); break;
+            }
+            break;
+        case 61:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QMediaPlaylist::PlaybackMode >(); break;
             }
             break;
         }
@@ -357,13 +438,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 50)
+        if (_id < 62)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 50;
+        _id -= 62;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 50)
+        if (_id < 62)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 50;
+        _id -= 62;
     }
     return _id;
 }
