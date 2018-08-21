0d3dd41d15bb7f9f6317400c36a1e12fd98b09ad
Pony Brown <31332409+JHSeng@users.noreply.github.com>
2018-08-21 23:52:41 +0800

    Renew connection.h

--- practiseProject/connection.h
+++ practiseProject/connection.h
@@ -9,6 +9,9 @@ static bool createConnection()
 {
     QSqlDatabase db=QSqlDatabase::addDatabase("QSQLITE");
     db.setDatabaseName("test.db");
+//    db.setHostName("jhseng");
+//    db.setUserName("jhseng");
+//    db.setPassword("1234");
     if (!db.open())
     {
         QMessageBox::critical(0,"cannot open database",
@@ -17,11 +20,14 @@ static bool createConnection()
         return false;
     }
     QSqlQuery query;
-    query.exec("create table student (id int primary key, "
+    query.exec("create table student (id int primary key, " //创造表
                "name varchar(20))");
     query.exec("insert into student values(0, 'LiMing')");
     query.exec("insert into student values(1, 'LiuTao')");
-    query.exec("insert into student values(2, 'WangHong')");
+    query.exec("insert into student values(2, 'WangHong')"); //插入数据
+
+    //明天再搞
+
     return true;
 }
 
